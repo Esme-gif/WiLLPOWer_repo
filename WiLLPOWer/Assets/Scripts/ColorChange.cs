28c92c89341c5b5b240f7f6414b57ce3b0966fec
Esme-gif <esmejuarez100@gmail.com>
2022-09-01 08:25:21 -0700

    working on Color change script

--- WiLLPOWer/Assets/Scripts/ColorChange.cs
+++ WiLLPOWer/Assets/Scripts/ColorChange.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class ColorChange : MonoBehaviour
 {
+    [Header("Colors")]
+    [SerializeField] private Material _OGColor;
+    [SerializeField] private Material _TriggerColor;
+    [SerializeField] private Material _PositionColor;
+
+
     // Start is called before the first frame update
     void Start()
     {
