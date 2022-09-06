cc1ee5c24edefda05801f5b8d632113f3c5c4f21
Esme-gif <esmejuarez100@gmail.com>
2022-09-06 08:23:23 -0700

    Color change a success
    Colorchange script works - need to do debugging to see what can go wrong
    
    next would be working on player movement for this scene of level 1

--- WiLLPOWer/Assets/Scripts/ColorChange.cs
+++ WiLLPOWer/Assets/Scripts/ColorChange.cs
@@ -6,13 +6,22 @@ public class ColorChange : MonoBehaviour
 {
     [Header("Colors")]
     [SerializeField] private Material _OGColor;
-    [SerializeField] private Material _TriggerColor;
+    [SerializeField] private Material _TriggerColor; // might make its own script
     [SerializeField] private Material _PositionColor;
+
+    [Header("Is Trap?")]
+    public bool isTrap;
+    
+
+    Renderer rend;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        rend = this.GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = _OGColor;
         
     }
 
@@ -21,4 +30,33 @@ public class ColorChange : MonoBehaviour
     {
         
     }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (isTrap)
+            {
+                rend.sharedMaterial = _TriggerColor;
+                // move player to starting point
+                //collision.gameObject.transform.position = new Vector3(5,2,0);
+            }
+            else
+            {
+                rend.sharedMaterial = _PositionColor;
+            }
+        }
+    }
+
+
+    void OnCollisionExit(Collision collisionInfo)
+    {
+        // it works Debug.Log("left the trap");
+        if (!isTrap)
+        {
+            rend.sharedMaterial = _OGColor;
+        }
+    }
+
+
 }
